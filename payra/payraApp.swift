50b4c8b259d452e0140edd369eecec6faaf76141
jayakrishnanm <jayakrishnan.m@neoito.com>
2025-07-01 02:41:27 +0530

    Initial Commit

---
+++ payra/payraApp.swift
@@ -0,0 +1,17 @@
+//
+//  payraApp.swift
+//  payra
+//
+//  Created by Jayakrishnan M on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct payraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
